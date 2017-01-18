9aaf70368724fc28d75c68395bf3b11ea0ee3e01
joe.wang <joe.wang@imaginato.com>
2017-01-18 08:19:30 +0700

    add IllegalArgumentException

---
+++ src/entity/exception/IllegalArgumentException.java
@@ -0,0 +1,9 @@
+package entity.exception;
+
+public class IllegalArgumentException extends RuntimeException{
+	
+	public IllegalArgumentException(String msg){
+		super(msg);
+	}
+	
+}
--- src/entity/move/Movement.java
+++ src/entity/move/Movement.java
@@ -1,6 +1,20 @@
 package entity.move;
 
+import entity.exception.IllegalArgumentException;
+
 public class Movement {
 	double start_x = 0.0;
 	double start_y = 0.0;
+	
+	double end_x = 0.0;
+	double end_y = 0.0;
+	
+	double velocity = 0.0;
+	double acceleration = 0.0;
+	
+	public Movement(double velocity, double acceleration, double[] start_end) throws IllegalArgumentException{
+		if(start_end.length!=4){
+			throw new IllegalArgumentException("Argument start_end should be an array with length of 4.");
+		}
+	}
 }
